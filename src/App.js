8c882dcc69f10c0eeae1d3c52b65a45c0e9e751a
Niccoryan0 <niccoryan0@gmail.com>
2020-10-07 14:09:19 -0700

    Creating Party Members component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PartyMembers from './PartyMembers'
 
-function App() {
-  var newobj = {
-    username : "Espresso401",
-    password : "@Test123!"
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      players: []
+    };
   }
-  fetch("https://localhost:44335/api/Account/Login", {
-    method: "post",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': "*"
-    },
-    body: JSON.stringify(newobj)
+
+  fetchApiStuff(){
+    var signIn = {
+      username : "Espresso401",
+      password : "@Test123!"
     }
-  ).then(res => res.json())
-   .then(result => {
-     if(result.jwt) localStorage.setItem('token', result.jwt)
-   })
+    fetch("https://localhost:44335/api/Account/Login", {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': "*"
+      },
+      body: JSON.stringify(signIn )
+      }
+    ).then(res => res.json())
+    .then(result => {
+      if(result.jwt) {
+        localStorage.setItem('token', result.jwt);
+      }
+    })
+  }
+
+  fetchPlayers(){
+
+  }
 
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   render(){
+     return (
+       <div className="App">
+         <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           {/* <p>
+             Edit <code>src/App.js</code> and save to reload.
+           </p>
+           <a
+             className="App-link"
+             href="https://reactjs.org"
+             target="_blank"
+             rel="noopener noreferrer"
+           >
+             Learn React
+           </a> */}
+         </header>
+         <main>
+           <PartyMembers players=></PartyMembers>
+         </main>
+       </div>
+     );
+   }
 }
 
 export default App;
--- src/PartyMembers.js
+++ src/PartyMembers.js
@@ -1,16 +1,50 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 
-function PartyMember(props){
-  return (
-    <div>
-      <img class="member-image" src="{props.ImageUrl}"></img>
-      <h2>{props.CharacterName}</h2>
-      <p>{props.Class}</p>
-      <p>{props.Race}</p>
-      <p>{props.ExperienceLevel}</p>
-      <p>{props.UserEmail}</p>
-    </div>
-  )
+class PartyMember extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      players: []
+    };
+  }
+
+  render(){
+      const pstyles = {
+        color: "red",
+      }
+      const imgstyles = {
+        height: "300px",
+        borderRadius: "50%"
+      }
+      return (
+        // <div>
+        //   <img class="member-image" src="{props.ImageUrl}"></img>
+        //   <h2>{props.CharacterName}</h2>
+        //   <p>{props.Class}</p>
+        //   <p>{props.Race}</p>
+        //   <p>{props.ExperienceLevel}</p>
+        //   <p>{props.UserEmail}</p>
+        // </div>
+        <CardDeck>
+          {this.state.players.map(function(player){
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="{player.ImageUrl}" />
+              <Card.Body>
+                <Card.Title>{player.CharacterName}</Card.Title>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>Class: {player.Class}</ListGroupItem>
+                <ListGroupItem>Race: {player.Race}</ListGroupItem>
+                <ListGroupItem>Experience Level: {player.ExperienceLevel}</ListGroupItem>
+                <ListGroupItem>Email: {player.UserEmail}</ListGroupItem>
+              </ListGroup>
+            </Card>
+          })}
+        </CardDeck>
+      )
+  }
 }
 
 
